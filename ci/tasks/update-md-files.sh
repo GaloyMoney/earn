abe4c377ab302832b83490840c1adcb3f4286d94
bodymindarts <justin@galoy.io>
2023-02-28 15:25:20 +0100

    ci: fix update-md-files task

--- ci/tasks/update-md-files.sh
+++ ci/tasks/update-md-files.sh
@@ -10,10 +10,7 @@ if [[ -z $(git config --global user.name) ]]; then
   git config --global user.name "CI Bot"
 fi
 
-(
-  cd $(git rev-parse --show-toplevel)
-  git merge --no-edit ${BRANCH}
-  git add -A
-  git status
-  git commit -m "chore: re-render markdown files"
-)
+git add -A
+git status
+
+git commit -m "chore: re-render markdown files"
