fe962d0bacd6ed12734b538d2efb1e760d82de43
bodymindarts <justin@galoy.io>
2023-02-28 14:47:55 +0100

    fix: bubble quiz

--- bin/update-md-files.sh
+++ bin/update-md-files.sh
@@ -6,6 +6,7 @@ find . -type f -path '*chapter*' -name '*.yml' | while read path; do
   file=$(basename "$path" .yml)
   new_path="$dir/$file.md"
 
+  echo "Templating $path to $new_path"
   handlebars-cli "$(cat $path | yq -o json)" ./quiz-page.hb > $new_path
 
 done
